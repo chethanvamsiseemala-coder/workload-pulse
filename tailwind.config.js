cce7ff73a4c29f77b1da06be9b8f9d2ae79e43bb
chethanvamsiseemala-coder <chethanvamsiseemala@gmail.com>
2026-09-11 08:13:19 +0530

    Fix DaisyUI plugin import in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// tailwind.config.js
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +11,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [
+    require('daisyui'),
+  ],
+};
